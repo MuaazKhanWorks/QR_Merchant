bc4dfaa78eb8f790c497b89a0e802e9eb1a96147
Ahmad Raza <chnirehan72@gmail.com>
2023-12-20 15:09:48 +0500

    push

--- src/main/java/com/mfs/merchantQR/model/TblMerchant.java
+++ src/main/java/com/mfs/merchantQR/model/TblMerchant.java
@@ -1,5 +1,7 @@
 package com.mfs.merchantQR.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.io.Serializable;
 import javax.persistence.*;
 import java.util.Date;
@@ -59,6 +61,7 @@ public class TblMerchant implements Serializable {
 
 	//bi-directional many-to-one association to LkpStatus
 	@ManyToOne
+	@JsonIgnore
 	@JoinColumn(name="STATUS_ID")
 	private LkpStatus lkpStatus;
 
